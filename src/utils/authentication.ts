ada61c5dc76a6326aab76a52bd5a11281db034d3
Xzavier Dunn <xmandunn@gmail.com>
2023-09-09 17:03:31 -0700

    request_user_authorization remove

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -32,51 +32,50 @@ async function get_access_token(
   return clientInfo;
 }
 
-async function request_user_authorization(
-  code: string,
-  credentials: CredentialsType
-) {
-  // RUN CONSOLE LOG TO GET CODE, THEN UPDATE CODE IN CONFIG.json
-  // console.log(
-  //   `https://accounts.spotify.com/authorize?client_id=${credentials.client_id}&response_type=code&redirect_uri=${credentials.redirect_uri}&scope=user-library-read user-library-modify user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played playlist-modify-public playlist-modify-private ugc-image-upload user-read-private user-read-email user-top-read user-follow-read user-follow-modify`
-  // );
-  // throw new Error("bruh");
+// TODO: Properly implement this
+// async function request_user_authorization(
+//   code: string,
+//   credentials: CredentialsType
+// ) {
+//   // console.log(
+//   //   `https://accounts.spotify.com/authorize?client_id=${credentials.client_id}&response_type=code&redirect_uri=${credentials.redirect_uri}&scope=user-library-read user-library-modify user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played playlist-modify-public playlist-modify-private ugc-image-upload user-read-private user-read-email user-top-read user-follow-read user-follow-modify`
+//   // );
+//   // throw new Error("");
 
-  // RUN THIS AFTER UPDATING CODE, THEN UPDATE user-stuff IN CONFIG.JSON
-  let formBody: string[] = [];
-  formBody.push(encodeURIComponent("code") + "=" + encodeURIComponent(code));
-  formBody.push(
-    encodeURIComponent("redirect_uri") +
-      "=" +
-      encodeURIComponent(credentials.redirect_uri)
-  );
-  formBody.push(
-    encodeURIComponent("grant_type") +
-      "=" +
-      encodeURIComponent("authorization_code")
-  );
+//   let formBody: string[] = [];
+//   formBody.push(encodeURIComponent("code") + "=" + encodeURIComponent(code));
+//   formBody.push(
+//     encodeURIComponent("redirect_uri") +
+//       "=" +
+//       encodeURIComponent(credentials.redirect_uri)
+//   );
+//   formBody.push(
+//     encodeURIComponent("grant_type") +
+//       "=" +
+//       encodeURIComponent("authorization_code")
+//   );
 
-  let finalBody: string = formBody.join("&");
+//   let finalBody: string = formBody.join("&");
 
-  let auth = Buffer.from(
-    credentials.client_id + ":" + credentials.client_secret
-  ).toString("base64");
-  let clientInfo = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + auth,
-    },
-    body: finalBody,
-  })
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((e) => {
-      throw e as Error;
-    });
+//   let auth = Buffer.from(
+//     credentials.client_id + ":" + credentials.client_secret
+//   ).toString("base64");
+//   let clientInfo = await fetch("https://accounts.spotify.com/api/token", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//       Authorization: "Basic " + auth,
+//     },
+//     body: finalBody,
+//   })
+//     .then((res) => res.json())
+//     .then((data) => data)
+//     .catch((e) => {
+//       throw e as Error;
+//     });
 
-  console.log(clientInfo);
-}
+//   console.log(clientInfo);
+// }
 
 async function refresh_user_access_token(
   refresh_token: string,
@@ -112,6 +111,6 @@ async function refresh_user_access_token(
 
 export {
   get_access_token,
-  request_user_authorization,
+  // request_user_authorization,
   refresh_user_access_token,
 };
